77fc6984a44df369e57a617cc1fb21ce1d7a3a9a
hakan.karanfil <hakan.karanfil@ALS-YG39104-J04>
2016-12-27 11:49:16 +0200

    devam

---
+++ opp_insankaynakları/opp_insankaynakları/calısan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opp_insankaynakları
+{
+    class calısan
+    {
+        public int sskno { get; set; }
+        public double maas { get; set; }
+        public override string ToString()
+        {
+            return string.Format("ssk no:{0} maas:{1}", sskno, maas);
+        }
+        public virtual double zamyap(double ekmaas)
+        {
+            return maas + ekmaas;
+            "kespt"
+        }
+    }
+}
---
+++ opp_insankaynakları/opp_insankaynakları/satıstemsilcisi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opp_insankaynakları
+{
+    class satıstemsilcisi:calısan
+    {
+        public int satismiktari { get; set; }
+        public override int zamyap(int satis)
+        {
+
+            return
+            
+        }
+    }
+}
