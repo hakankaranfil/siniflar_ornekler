dc95c43d36013f747a727f8fcbea38046eca81eb
hakan.karanfil <hakan.karanfil@ALS-YG39104-J04>
2017-01-18 11:33:23 +0200

    veri çekme

--- SQL/SQL/Form1.cs
+++ SQL/SQL/Form1.cs
@@ -22,11 +22,20 @@ namespace SQL
         private void Form1_Load(object sender, EventArgs e)
         {
             SqlConnection baglan = new SqlConnection("Server=ALS-YG39104-J04;Database=Northwind;UID=sa;PWD=123456789?");//mssql connection string
-            SqlCommand cmd = new SqlCommand("INSERT INTO Categories(CategoryName,Description) values ('Yeni kategori','açıklama')", baglan);
-            baglan.Open();
-            cmd.ExecuteNonQuery();
-            baglan.Close();
-            baglan.Dispose();
+            //SqlCommand cmd = new SqlCommand("INSERT INTO Categories(CategoryName,Description) values ('Yeni kategori','açıklama')", baglan);
+            //baglan.Open();
+            //cmd.ExecuteNonQuery();
+            
+            //baglan.Close();
+            //baglan.Dispose();
+            SqlDataAdapter dap = new SqlDataAdapter("SELECT * FROM Ogrenci", baglan);
+            DataTable dt = new DataTable();
+            dap.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            
+
+            
 
         }
     }
