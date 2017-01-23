1e8a1b118089a408c19275ffd63a1d0429c6dbe6
hakan.karanfil <hakan.karanfil@ALS-YG39104-J04>
2017-01-23 11:22:07 +0200

    sql bağlantı

---
+++ spor merkezi/spor merkezi/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace spor_merkezi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtkayıtno = new System.Windows.Forms.TextBox();
+            this.txtadsoyad = new System.Windows.Forms.TextBox();
+            this.txtucret = new System.Windows.Forms.TextBox();
+            this.cmbsure = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btngöster = new System.Windows.Forms.Button();
+            this.btnkaydet = new System.Windows.Forms.Button();
+            this.btnsil = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kayıt no:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AdSoyad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Süre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ücret:";
+            // 
+            // txtkayıtno
+            // 
+            this.txtkayıtno.Location = new System.Drawing.Point(104, 24);
+            this.txtkayıtno.Name = "txtkayıtno";
+            this.txtkayıtno.Size = new System.Drawing.Size(100, 20);
+            this.txtkayıtno.TabIndex = 4;
+            // 
+            // txtadsoyad
+            // 
+            this.txtadsoyad.Location = new System.Drawing.Point(104, 69);
+            this.txtadsoyad.Name = "txtadsoyad";
+            this.txtadsoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtadsoyad.TabIndex = 5;
+            // 
+            // txtucret
+            // 
+            this.txtucret.Location = new System.Drawing.Point(104, 139);
+            this.txtucret.Name = "txtucret";
+            this.txtucret.Size = new System.Drawing.Size(100, 20);
+            this.txtucret.TabIndex = 6;
+            // 
+            // cmbsure
+            // 
+            this.cmbsure.FormattingEnabled = true;
+            this.cmbsure.Items.AddRange(new object[] {
+            "1",
+            "3",
+            "6",
+            "9",
+            "12"});
+            this.cmbsure.Location = new System.Drawing.Point(104, 102);
+            this.cmbsure.Name = "cmbsure";
+            this.cmbsure.Size = new System.Drawing.Size(100, 21);
+            this.cmbsure.TabIndex = 7;
+            this.cmbsure.SelectedIndexChanged += new System.EventHandler(this.cmbsure_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 228);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(430, 154);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // btngöster
+            // 
+            this.btngöster.Location = new System.Drawing.Point(234, 34);
+            this.btngöster.Name = "btngöster";
+            this.btngöster.Size = new System.Drawing.Size(96, 32);
+            this.btngöster.TabIndex = 9;
+            this.btngöster.Text = "AboneleriGöster";
+            this.btngöster.UseVisualStyleBackColor = true;
+            this.btngöster.Click += new System.EventHandler(this.btngöster_Click);
+            // 
+            // btnkaydet
+            // 
+            this.btnkaydet.Location = new System.Drawing.Point(234, 91);
+            this.btnkaydet.Name = "btnkaydet";
+            this.btnkaydet.Size = new System.Drawing.Size(96, 32);
+            this.btnkaydet.TabIndex = 10;
+            this.btnkaydet.Text = "Kaydet";
+            this.btnkaydet.UseVisualStyleBackColor = true;
+            this.btnkaydet.Click += new System.EventHandler(this.btnkaydet_Click);
+            // 
+            // btnsil
+            // 
+            this.btnsil.Location = new System.Drawing.Point(224, 139);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(96, 32);
+            this.btnsil.TabIndex = 11;
+            this.btnsil.Text = "SİL";
+            this.btnsil.UseVisualStyleBackColor = true;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(489, 382);
+            this.Controls.Add(this.btnsil);
+            this.Controls.Add(this.btnkaydet);
+            this.Controls.Add(this.btngöster);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmbsure);
+            this.Controls.Add(this.txtucret);
+            this.Controls.Add(this.txtadsoyad);
+            this.Controls.Add(this.txtkayıtno);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtkayıtno;
+        private System.Windows.Forms.TextBox txtadsoyad;
+        private System.Windows.Forms.TextBox txtucret;
+        private System.Windows.Forms.ComboBox cmbsure;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btngöster;
+        private System.Windows.Forms.Button btnkaydet;
+        private System.Windows.Forms.Button btnsil;
+    }
+}
+
---
+++ spor merkezi/spor merkezi/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace spor_merkezi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        SqlConnection baglan = new SqlConnection("Server=ALS-YG39104-J04;Database=Northwind;UID=sa;PWD=123456789?");
+
+        
+        private void btngöster_Click(object sender, EventArgs e)
+        {
+            
+
+            baglan.Open();
+            SqlDataAdapter dap = new SqlDataAdapter("SELECT*FROM Musteri", baglan);
+            DataTable dt = new DataTable();
+            dap.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglan.Close();
+        }
+
+        private void btnkaydet_Click(object sender, EventArgs e)
+        {
+            int kayitno = int.Parse(txtkayıtno.Text);
+            string adsoyad = txtadsoyad.Text;
+            string sure = (cmbsure.SelectedItem.ToString());
+            string ucret = (txtucret.Text);
+            baglan.Open();
+            SqlCommand kmt = new SqlCommand("INSERT INTO Musteri (kayitno,Adisoyadi,Sure,Ucret) VALUES('" + kayitno + " ',' " + adsoyad + "',' " + sure + "',' " + ucret + "')", baglan);
+            kmt.ExecuteNonQuery();
+            baglan.Close();
+
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void cmbsure_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbsure.SelectedItem.ToString() == "1")
+            {
+                txtucret.Text = "100";
+            }
+            else if (cmbsure.SelectedItem.ToString() == "3")
+            {
+                txtucret.Text = "250";
+            }
+            else if (cmbsure.SelectedItem.ToString() == "6")
+            {
+                txtucret.Text = "450";
+            }
+            else if (cmbsure.SelectedItem.ToString() == "9")
+            {
+                txtucret.Text = "600";
+            }
+            else
+            {
+                txtucret.Text = "850";
+            }
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            int kayitid = int.Parse(txtkayıtno.Text);
+            SqlCommand kmt = new SqlCommand("DELETE FROM Musteri where kayitno="+kayitid, baglan);
+            kmt.ExecuteNonQuery();
+            baglan.Close();
+        }
+    }
+}
