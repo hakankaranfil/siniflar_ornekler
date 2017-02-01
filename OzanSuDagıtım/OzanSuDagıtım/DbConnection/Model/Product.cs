f52a871cc484ef501595fa3887568981745ba610
hakan.karanfil <hakan.karanfil@ALS-YG39104-J04>
2017-02-01 11:41:18 +0200

    qa

---
+++ OzanSuDagıtım/OzanSuDagıtım/DbConnection/Model/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OzanSuDagıtım.DbConnection.Model
+{
+  public  class Product
+    {
+        public int ProductID  { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<Double> ProductPrice { get; set; }
+
+    }
+}
