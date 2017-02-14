a4af0ff02c907a2722fabbfaafede0a74d0db5fe
hakan.karanfil <hakan.karanfil@ALS-YG39104-J04>
2017-02-14 11:52:36 +0200

    finish

---
+++ JS_Chat/JS_Chat/Default.js
@@ -0,0 +1,23 @@
+﻿var kisilistesi = [];
+var kisiekle=function()
+    {
+    var kisi = {};
+    kisi.ad = get("txtadsoyad").value;
+    if (kisi.ad == "")
+    {
+        alert("uygun formatta isim giriniz")
+    }
+    else 
+    {
+        kisi.email = get("txtemail").value;
+        alert("uygun formatta isim giriniz")
+        }
+    kisi.bilgileri = function () {
+        return kisi.ad + " " + kisi.email;
+    }
+    kisilistesi.push(kisi);
+    get("kisiler").innerHTML += kisi.bilgileri() + "</br>";
+}
+function get(elementID) {
+    return document.getElementById(elementID);
+}
---
+++ JS_Chat/JS_Chat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JS_Chat.Global" Language="C#" %>
